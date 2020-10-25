aae4719051ea91a28bd46d609fe134865d8f08a5
Himanshu <raihimanshu22345@gmail.com>
2020-10-25 15:14:50 +0530

    added ui

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Deaths from './components/Deaths';
 import QuotesContainer from './components/QuotesContainer';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const Home = ()=>{
   return(
     <div className="App">
       <h1>Home</h1>
-      /deaths for deaths<br/>
-      /quotes for quotes
     </div>
   )
 }
@@ -17,6 +16,7 @@ function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar/>
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path = "/quotes" component = {QuotesContainer}/>
